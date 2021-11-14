415966b0019cc7fd1d96ae8594df2ac8053271df
keimos <keithmosleyjr@gmail.com>
2021-11-13 19:58:30 -0500

    Updated CurrencyTest.java and Dollar.java

---
+++ src/test/java/com/mosley/interview/CurrencyTest.java
@@ -0,0 +1,23 @@
+package com.mosley.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class CurrencyTest {
+    @Test
+    void testCurrency() {
+        Dollar dollar = new Dollar(5);
+        Dollar product = dollar.times(2);
+        assertEquals(10, product.amount);
+        product = dollar.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+}
